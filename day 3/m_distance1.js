5ecd49d9f35b7487ece463ba28bc8d6c7774eef2
connor <cwebdale006@west-mec.org>
2020-03-17 23:14:45 -0700

    well
    not 100% how to solve this, im going to start by trying to create a visual of what is happening

---
+++ day 3/m_distance1.js
@@ -0,0 +1,10 @@
+/**
+ * Author: Connor J Webdale 
+ * Date: 3.17.20
+ * 
+ * filename: m_distance1.js
+ */
+
+// declaring two variables as an array, to use as coordinates
+let firstWire = (0,0);
+let secondWire = (0,0);
